e37de1aa2c54627eff3fdc7a2f0ad81a933199b1
Elena Murphy <emurphy12@luc.edu>
2017-09-11 22:48:06 +0000

    lab1-part3
    Signed-off-by: Elena Murphy <emurphy12@luc.edu>

--- fizzbuzz/src/main/java/hw/fizzbuzz.java
+++ fizzbuzz/src/main/java/hw/fizzbuzz.java
@@ -1,6 +1,6 @@
 package hw;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class fizzbuzz {
 
@@ -35,11 +35,52 @@ public class fizzbuzz {
 
   // PART THREE
 
-  public static String[] fizzBuzz(final int n) {
+  // public static String[] fizzBuzz(int n) {
 
+  //   if (n >= 1) {
+
+  //     String[] list = new String[n];
+
+  //     for (int i = 1; i <= n; i++) {
+  //       int x = i % 3;
+  //       int y = i % 5;
+
+  //       if (x == 0 && y == 0) {
+
+  //         list[i] = "fizz buzz";
+
+  //       } else if (x == 0) {
+
+  //         list[i] = "fizz";
+
+  //       } else if (y == 0) {
+
+  //         list[i] = "buzz";
+
+  //       } else {
+
+  //         list[i] = String.valueOf(i);
+  //       }
+        
+  //     }
+      
+  //     return list;
+
+  //   } else {
+      
+  //     System.out.println("Invalid");
+  //     return null;
+  //   }
+    
+  // }
+  
+  // PART FOUR
+
+  public static ArrayList<String> fizzBuzz(int n) {
+    
     if (n >= 1) {
-
-      String[] list = new String[n];
+      
+      ArrayList<String> list = new ArrayList<String>(n);
 
       for (int i = 1; i <= n; i++) {
         int x = i % 3;
@@ -47,29 +88,29 @@ public class fizzbuzz {
 
         if (x == 0 && y == 0) {
 
-          list[i] = "fizz buzz";
+          list.add("fizz buzz");
 
         } else if (x == 0) {
 
-          list[i] = "fizz";
+          list.add("fizz");
 
         } else if (y == 0) {
 
-          list[i] = "buzz";
+          list.add("buzz");
 
         } else {
 
-          list[i] = String.valueOf(i);
+          list.add(String.valueOf(i));
         }
-        
       }
-      
+
       return list;
 
     } else {
       
       System.out.println("Invalid");
       return null;
+      
     }
     
   }
