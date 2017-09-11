1756f04d5d82ad8c7282acbe5e8f9b54c30b10cc
Elena Murphy <emurphy12@luc.edu>
2017-09-11 04:47:32 +0000

    working on part 2
    Signed-off-by: Elena Murphy <emurphy12@luc.edu>

--- fizzbuzz/src/main/java/hw/fizzBuzz.java
+++ fizzbuzz/src/main/java/hw/fizzBuzz.java
@@ -3,30 +3,44 @@ package hw;
 public class fizzBuzz {
     public static void main(final String[] args) {
         
-        for (int i = 1; i <= args.length; i++) {
+        int n = Integer.parseInt(args[0]);
+        
+        String[] returnArray = fizzBuzzArray(n);
+        
+        return returnArray;
+
+    }
+    
+    public static String[] fizzBuzzArray(final int n) {
+        
+        String[] list = new String[n];
+        
+        for (int i = 1; i <= n; i++) {
             int x = i % 3;
             int y = i % 5;
             
             if (x == 0 && y == 0) {
                 
-                System.out.println("fizz buzz");
+                list[i]="fizz buzz";
                 
             } else if (x == 0) {
                 
-                System.out.println("fizz");
+                list[i]="fizz";
                 
             } else if (y == 0) {
                 
-                System.out.println("buzz");
+                list[i]="buzz";
                 
             } else {
                 
-                System.out.println(i);
+                list[i]=Integer.toString(i);
                 
             }
 
         }
-
+        
+        return list;
+        
     }
     
     
