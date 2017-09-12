52dccd5cb78d869a0c69766eb3491355587b2287
Elena Murphy <emurphy12@luc.edu>
2017-09-12 03:10:45 +0000

    lab1-part5
    Signed-off-by: Elena Murphy <emurphy12@luc.edu>

--- fizzbuzz/src/main/java/hw/fizzbuzz.java
+++ fizzbuzz/src/main/java/hw/fizzbuzz.java
@@ -4,77 +4,83 @@ import java.util.*;
 
 public class fizzbuzz {
 
-  // PART TWO
+  /** PART TWO
 
-  // public static void printFizzBuzz(int n) {
+  public static void printFizzBuzz(int n) {
 
-  //   for (int i = 1; i <= n; i++) {
-  //     int x = i % 3;
-  //     int y = i % 5;
+    for (int i = 1; i <= n; i++) {
+      int x = i % 3;
+      int y = i % 5;
 
-  //     if (x == 0 && y == 0) {
+      if (x == 0 && y == 0) {
 
-  //       System.out.println("fizz buzz");
+        System.out.println("fizz buzz");
 
-  //     } else if (x == 0) {
+      } else if (x == 0) {
 
-  //       System.out.println("fizz");
+        System.out.println("fizz");
 
-  //     } else if (y == 0) {
+      } else if (y == 0) {
 
-  //       System.out.println("buzz");
+        System.out.println("buzz");
 
-  //     } else {
+      } else {
 
-  //       System.out.println(i);
-  //     }
+        System.out.println(i);
+      }
       
-  //   }
+    }
 
-  // }
-
-  // PART THREE
-
-  // public static String[] fizzBuzz(int n) {
-
-  //   if (n >= 1) {
-
-  //     String[] list = new String[n];
-
-  //     for (int i = 1; i <= n; i++) {
-  //       int x = i % 3;
-  //       int y = i % 5;
-
-  //       if (x == 0 && y == 0) {
-
-  //         list[i] = "fizz buzz";
-
-  //       } else if (x == 0) {
-
-  //         list[i] = "fizz";
-
-  //       } else if (y == 0) {
-
-  //         list[i] = "buzz";
-
-  //       } else {
-
-  //         list[i] = String.valueOf(i);
-  //       }
-        
-  //     }
-      
-  //     return list;
-
-  //   } else {
-      
-  //     System.out.println("Invalid");
-  //     return null;
-  //   }
-    
-  // }
+  }
   
-  // PART FOUR
+  **/
+
+  /** PART THREE
+
+   public static String[] fizzBuzz(int n) {
+
+    if (n >= 1) {
+
+      String[] list = new String[n];
+
+      for (int i = 1; i <= n; i++) {
+        int x = i % 3;
+        int y = i % 5;
+
+        if (x == 0 && y == 0) {
+
+          list[i] = "fizz buzz";
+
+        } else if (x == 0) {
+
+          list[i] = "fizz";
+
+         } else if (y == 0) {
+
+           list[i] = "buzz";
+
+         } else {
+
+           list[i] = String.valueOf(i);
+         }
+        
+       }
+      
+       return list;
+
+     } else {
+      
+       System.out.println("Invalid");
+       return null;
+     }
+    
+   }
+   
+  **/
+   
+  /**
+  
+  // PART FOUR */
 
   public static ArrayList<String> fizzBuzz(int n) {
     
